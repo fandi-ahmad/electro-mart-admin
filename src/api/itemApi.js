90d8b01cf497a309587470de6087e0b2fb8dfd83
fandi-ahmad <fandi4160@gmail.com>
2023-04-26 21:09:27 +0800

    auth token

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,10 +1,21 @@
 import axios from "axios";
-// const apiUrl = process.env.API_URL;
 const apiUrl = 'http://127.0.0.1:3333'
+const token = 'MQ.m4JtWeDxHh4DCNL0z1uTOd8Wu-WJFUV0vLVP02ISdDjMeE9WUNxMFsewmmjJ'
 
+export const LoginUser = () => {
+    return axios.post(`${apiUrl}/api/login`)
+    .then(response => response.data)
+    .catch(error => {
+        throw error;
+    });
+}
+
+const headers = {
+    'Authorization': 'Bearer ' + token
+};
 
 export const GetItem = (page, limit) => {
-    return axios.get(`${apiUrl}/api/item?page=${page}&limit=${limit}`,)
+    return axios.get(`${apiUrl}/api/item?page=${page}&limit=${limit}`, {headers})
     .then(response => response.data)
     .catch(error => {
         throw error;
