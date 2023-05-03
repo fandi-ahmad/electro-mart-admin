e88c618669415c85ca79c7d4391a1b0e1a9dabb1
fandi-ahmad <fandi4160@gmail.com>
2023-05-03 21:47:34 +0800

    route condition if user login

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { React, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
 import Item from "../pages/Item";
 import Login from "../pages/Login";
 
 const RoutesTemplate = () => {
+
+  const checkUser = () => {
+    if (localStorage.getItem('userToken') == null && window.location.pathname !== '/login') {
+      window.location.href = '/login'
+    } else if (localStorage.getItem('userToken') != null && window.location.pathname === '/login') {
+      window.location.href = '/'
+    }
+  }
+
+  useEffect(() => {
+    checkUser()
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard/>} />
         <Route path="/item" element={<Item/>} />
         <Route path="/login" element={<Login/>} />
+        {/* <Route path="*" element={<Navigate to="/" />} /> */}
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default RoutesTemplate
+export default RoutesTemplate;
